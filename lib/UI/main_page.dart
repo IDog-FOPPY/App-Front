680f8b1e5e0c54a5e347c17c323a367aa4893e3f
Jihyun_Kim <jihyun3313@daum.net>
2023-10-13 00:17:52 +0900

    메인페이지 퍼블리싱 진행중

--- lib/UI/main_page.dart
+++ lib/UI/main_page.dart
@@ -1,7 +1,8 @@
 import "package:flutter/material.dart";
 import "package:foppy_front/UI/drawer_page.dart";
 
-class MainPage extends StatefulWidget {   //즉각적인 변화를 반영하는 페이지이므로 stf 사용
+class MainPage extends StatefulWidget {
+  //즉각적인 변화를 반영하는 페이지이므로 stf 사용
   const MainPage({Key? key}) : super(key: key);
 
   @override
@@ -9,23 +10,21 @@ class MainPage extends StatefulWidget {   //즉각적인 변화를 반영하는 
 }
 
 class _MainPageState extends State<MainPage> {
-
-
-  Widget yellowBox(Size screenSize){
+  Widget yellowBox(Size screenSize) {
     return Stack(
       children: [
         Container(
-          margin: EdgeInsets.only(top: 20),   //위쪽 방향으로 여백
-          width: screenSize.width * 0.85,
-          height: screenSize.width * 0.6,
-          decoration: BoxDecoration(borderRadius: BorderRadius.circular(30)),
-          child:FittedBox(
-              fit: BoxFit.fill,
-              child: Image.asset('assets/Image/banner.png')
-          )
-        ),
+            margin: EdgeInsets.only(top: 20), //위쪽 방향으로 여백
+            width: screenSize.width * 0.85,
+            height: screenSize.width * 0.6,
+            decoration: BoxDecoration(borderRadius: BorderRadius.circular(30)),
+            child: FittedBox(
+                fit: BoxFit.fill,
+                child: Image.asset('assets/Image/banner.png'))),
         Positioned(
-            bottom:0, top: 0, left: 25, //컨테이너의 중앙에 오게끔
+            bottom: 0,
+            top: 0,
+            left: 25, //컨테이너의 중앙에 오게끔
             child: Container(
               width: screenSize.width * 0.2,
               height: screenSize.width * 0.05,
@@ -33,27 +32,69 @@ class _MainPageState extends State<MainPage> {
                   fit: BoxFit.fitWidth,
                   child: InkWell(
                     onTap: null,
-                    child: Image.asset('assets/Image/img1.png'),    //비문이 뭔가요? 부분
-                  )
-              ),
-            )
-        ),
+                    child: Image.asset('assets/Image/img1.png'), //비문이 뭔가요? 부분
+                  )),
+            )),
         Positioned(
-          bottom: 20, left: 25,
+            bottom: 20,
+            left: 25,
             child: Container(
               width: screenSize.width * 0.4,
               height: screenSize.width * 0.12,
               child: FittedBox(
-                fit: BoxFit.fill,
-                child: InkWell(
-                  onTap: null,
-                  child: Image.asset('assets/Image/finding.png'),
-                )
-              ),
-            )
-        )
+                  fit: BoxFit.fill,
+                  child: InkWell(
+                    onTap: null,
+                    child: Image.asset('assets/Image/finding.png'),
+                  )),
+            ))
       ],
     );
+  }
+
+  Widget addMyDogBox(Size screenSize) {
+    return Column(children: [
+      // 타이틀 섹션
+      Container(
+          margin: EdgeInsets.only(top: 30), //위쪽 방향으로 여백
+          width: screenSize.width * 0.85,
+          child: Row(
+            children: [
+              Container(
+                  child: Image.asset('assets/Image/paw.png',
+                      height: 20, width: 20)),
+              Container(
+                          margin: EdgeInsets.only(left: 10), //위쪽 방향으로 여백
+
+                child: Text('내 반려견을 등록할게요'))
+            ],
+          )),
+
+      // 반려견 카드 섹션
+      Column(
+        children: [
+          Container(
+              // 임의로 그냥 넣어둠
+              margin: EdgeInsets.only(top: 20), //위쪽 방향으로 여백
+              width: screenSize.width * 0.85,
+              height: screenSize.width * 0.6,
+              decoration:
+                  BoxDecoration(borderRadius: BorderRadius.circular(30)),
+              child: FittedBox(
+                  fit: BoxFit.fill,
+                  child: Image.asset('assets/Image/banner.png'))),
+          Container(
+              margin: EdgeInsets.only(top: 20), //위쪽 방향으로 여백
+              width: screenSize.width * 0.85,
+              height: screenSize.width * 0.6,
+              decoration:
+                  BoxDecoration(borderRadius: BorderRadius.circular(30)),
+              child: FittedBox(
+                  fit: BoxFit.fill,
+                  child: Image.asset('assets/Image/banner.png'))),
+        ],
+      )
+    ]);
   }
 
   @override
@@ -63,19 +104,18 @@ class _MainPageState extends State<MainPage> {
     return Scaffold(
       appBar: AppBar(
         iconTheme: IconThemeData(color: Colors.black38),
-        backgroundColor: Colors.transparent,  //앱바 배경 투명
-        elevation: 0,   //그림자 농도 0으로 설정
+        backgroundColor: Colors.transparent, //앱바 배경 투명
+        elevation: 0, //그림자 농도 0으로 설정
       ),
-      endDrawer: Drawer(
-        child: MenuDrawer()
-      ),
+      endDrawer: Drawer(child: MenuDrawer()),
       body: SingleChildScrollView(
-        scrollDirection: Axis.vertical,   //세로 스크롤
+        scrollDirection: Axis.vertical, //세로 스크롤
         child: SizedBox(
           child: Center(
             child: Column(
               children: [
                 yellowBox(screenSize),
+                addMyDogBox(screenSize),
               ],
             ),
           ),
